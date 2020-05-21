b9ed9bc4109ae3290a520fe870533e152bf93048
Kilian <kilian.paquier@outlook.fr>
2020-05-21 19:03:08 +0200

    amélioration register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import { first } from 'rxjs/operators';
+import {first} from 'rxjs/operators';
 
-import { User } from '../entities';
-import { UserService, AlertService } from '../services';
+import {User} from '../entities';
+import {UserService, AlertService} from '../services';
 
 @Component({
   selector: 'app-register',
@@ -18,28 +18,31 @@ export class RegisterComponent implements OnInit {
   submitted = false;
 
   constructor(private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private userService: UserService,
-    private alertService: AlertService) { }
+              private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService,
+              private alertService: AlertService) {
+  }
 
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      mail: ['', Validators.email],
+      mail: ['', [Validators.email, Validators.required]],
       tel: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: ['', [Validators.required, Validators.minLength(2)]],
       vehicle: ['', Validators.nullValidator],
       seats: ['', [Validators.min(1), Validators.max(10)]],
-      baggage: ['', Validators.required],
+      baggage: ['', Validators.nullValidator],
       talk: ['', Validators.required],
       smoke: ['', Validators.required],
     });
   }
-  
+
   onSubmit(): void {
     this.submitted = true;
 
@@ -51,20 +54,20 @@ export class RegisterComponent implements OnInit {
       return;
     }
 
-    let user:User = this.form.value;
+    let user: User = this.form.value;
 
     this.loading = true;
     this.userService.addUser(user)
       .pipe(first())
       .subscribe(
         data => {
-          this.alertService.success('Registration successful', { keepAfterRouteChange: true });
-          this.router.navigate(['../login'], { relativeTo: this.route });
+          this.alertService.success('Registration successful', {keepAfterRouteChange: true});
+          this.router.navigate(['../login'], {relativeTo: this.route});
         },
         error => {
           this.alertService.error(error);
           this.loading = false;
         });
   }
-  
+
 }
