b7fc11673d0dda85facd68a8ae439c8bb9f6ccde
Gableb <gableb@hotmail.fr>
2020-05-22 01:16:01 -0400

    login component should work w/ proper api response

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { first } from 'rxjs/operators';
+
+import { environment } from '@environments/environment'
+
+import { User } from '@app/entities';
+import { UserService, AlertService } from '@app/services';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService,
+    private alertService: AlertService
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      mail: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.login(this.f.mail.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.loading = false;
+          if (data !== undefined){//login failed
+            this.router.navigate([this.returnUrl]);
+          }
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
   }
 
 }
